7236abe2172e55192cea7e526da51d19ae940a44
Roman <romanuch06.88@gmail.com>
2022-10-31 21:38:38 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { App } from './App';
 import './index.css';
+import App from './App';
 
 const root = document.getElementById('root');
 // const root = ReactDOM.createRoot(document.getElementById('root'));
